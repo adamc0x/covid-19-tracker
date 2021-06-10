0b7f928a6a6047534bb682b9987bfb41e0b4ed9d
Adam <adammcgiolla@gmail.com>
2021-06-10 13:07:00 +0100

    Add files via upload

---
+++ src/Map.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function map() {
+    return (
+        <div className="map">
+            <h1>map</h1>
+        </div>
+    );
+}
+
+export default map
---
+++ src/Table.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Table.css';
+
+function Table({countries}) {
+    return (
+        <div className="table">
+            {countries.map((country, cases) => (
+                <tr>
+                    <td>{country.country}</td>
+                    <strong>{(country.cases)}</strong>
+                </tr>
+            ))}
+        </div>
+    );
+}
+
+export default Table;
+
