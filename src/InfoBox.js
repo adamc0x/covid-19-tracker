bc1d71bbe1f5692160d43bdd39d271c56f681600
Adam <adammcgiolla@gmail.com>
2021-06-13 14:59:28 +0100

    Add files via upload

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,20 +1,36 @@
-import React from 'react'
+import React from "react";
 import { Card, CardContent, Typography } from "@material-ui/core";
+import "./InfoBox.css";
+function InfoBox({ title, isRed, isGrey, active, cases, total, ...props }) {
+  return (
+    <Card
+      onClick={props.onClick}
+      className={`infoBox ${active && "infoBox--selected"} ${
+        isRed && "infoBox--red"
+      } ${isGrey && "infoBox--grey"}`}
+    >
+      <CardContent>
+        {/* Title */}
+        <Typography className="infoBox__title" color="textSecondary">
+          {title}
+        </Typography>
 
-function InfoBox({title, cases, total}) {
-    return (
-        <Card className="infoBox">
-            <CardContent>
+        {/* Number of Cases */}
+        <h2
+          className={`infoBox__cases ${!isRed && "infoBox__cases--green"} ${
+            isGrey && "infoBox__cases--grey"
+          }`}
+        >
+          {props.isloading ? <i className="fa fa-cog fa-spin fa-fw" /> : cases}
+        </h2>
 
-                <Typography className = "infoBox__title" color="textSecondary"> {title} </Typography>
-                
-                <h2 className="infoBox__cases"> {cases} </h2>
-
-                <Typography className = "infoBox__total" color="textSecondary"> {total} Total </Typography>
-
-            </CardContent>
-        </Card>
-    )
+        {/* Total Cases */}
+        <Typography className="infoBox__total" color="textSecondary">
+          {total} Total
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
 
-export default InfoBox
+export default InfoBox;
--- src/Table.js
+++ src/Table.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import './Table.css';
+import React from "react";
+import "./Table.css";
+import numeral from "numeral";
 
-function Table({countries}) {
-    return (
-        <div className="table">
-            {countries.map((country, cases) => (
-                <tr>
-                    <td>{country.country}</td>
-                    <strong>{(country.cases)}</strong>
-                </tr>
-            ))}
-        </div>
-    );
+function Table({ countries }) {
+  return (
+    <div className="table">
+      {countries.map(({ country, cases }) => (
+        <tr>
+          <td>{country}</td>
+          <td>
+            <strong>{numeral(cases).format("000,000")}</strong>
+          </td>
+        </tr>
+      ))}
+    </div>
+  );
 }
 
 export default Table;
-
